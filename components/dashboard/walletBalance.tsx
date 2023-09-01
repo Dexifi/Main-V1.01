7dd07a07ac9e08bae90877e0342a8aedfc187503
Dexifi <dexifi.finance@gmail.com>
2023-09-01 13:32:50 +0330

    Update walletBalance.tsx

--- components/dashboard/walletBalance.tsx
+++ components/dashboard/walletBalance.tsx
@@ -180,10 +180,10 @@ const WalletBalance: FC<WalletBalanceProps> = ({
                       {item.uiAmount.toFixed(2)}
                     </div>
                     <div className={styles.div28}>
-                      $ {item.value.toFixed(2)}
+                      $ {item.value}
                     </div>
                     <div className={styles.div29}>
-                      {Number(item.price.toFixed(5))}
+                      {Number(item.price)}
                     </div>
                     <div className={styles.div30}>
                       {walletBalance
