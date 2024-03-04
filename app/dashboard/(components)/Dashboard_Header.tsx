03ec259b9921841717e12e7e60231bb69ba28a53
Hamid Reza Lashgari <73655719+HamidrezaLashgari@users.noreply.github.com>
2024-03-04 16:51:51 +0330

    feat: add main tab

--- app/dashboard/(components)/Dashboard_Header.tsx
+++ app/dashboard/(components)/Dashboard_Header.tsx
@@ -1,42 +1,23 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import formatedString from "@/lib/string";
-import { cn } from "@/lib/utils";
 import { DefaultTab, NFT_Gallery, TransactionsTab } from "./(tabs)";
 import AccountsTab from "./(tabs)/AccountsTab";
-import { ChevronFirst } from "lucide-react";
 import { useState } from "react";
 
-type Props = {
-  isMobile: boolean;
-};
+const tabs = ["Main", "NFT Gallery", "Transactions", "Accounts"];
 
-const tabs = ["NFT Gallery", "Transactions", "Accounts"];
-
-const DashboardHeader = ({ isMobile }: Props) => {
-  const [page, setPage] = useState("");
+const DashboardHeader = () => {
+  const [page, setPage] = useState("main");
   return (
     <div className="flex justify-between items-center w-full relative z-50">
       <Tabs className="w-full bg-transparent" defaultValue="default">
-        <TabsList
-          className={`flex justify-between items-center flex-wrap gap-4 h-max`}
-        >
-          <TabsTrigger
-            value="default"
-            className="flex rounded-full hover:bg-[#D9F8FF20] md:flex justify-center items-center box-border px-0 min-w-8 min-h-8 sm:px-7 text-white text-[0px] sm:text-sm"
-            style={{
-              border: "1px solid #D9F8FF",
-              boxShadow: "0 0 4px 1px #d9f8ff75",
-            }}
-          >
-            <ChevronFirst className="text-muted w-4 h-4 aspect-square object-contain" />
-            Back
-          </TabsTrigger>
-          <div className="flex gap-0 sm:gap-x-2 z-10 bg-[#0d111b] rounded-full">
+        <TabsList className={`flex  items-center flex-wrap gap-4 h-max`}>
+          <div className="flex gap-0 sm:gap-x-2 z-10 bg-[#0d111b] rounded-full ">
             {tabs.map((tab, index) => (
               <TabsTrigger
                 value={formatedString(tab.toLocaleLowerCase())}
                 key={index}
-                className="text-xs sm:text-sm md:text-lg text-center hyphens-none flex md:flex px-3 sm:px-5 py-2 w-max cursor-pointer font-['DM Sans'] text-[#d9f8ff] hover:text-white transition-all box-border data-[state=active]:bg-[#D9F8FF10]  rounded-full transition-none shadow-none border-none"
+                className="text-xs sm:text-sm md:text-lg text-center hyphens-none flex md:flex px-2 sm:px-5 py-2 w-max cursor-pointer font-['DM Sans'] text-[#d9f8ff] hover:text-white transition-all box-border data-[state=active]:bg-[#D9F8FF10]  rounded-full transition-none shadow-none border-none"
                 onClick={() => setPage(formatedString(tab.toLocaleLowerCase()))}
                 style={{
                   border:
@@ -56,7 +37,7 @@ const DashboardHeader = ({ isMobile }: Props) => {
         </TabsList>
 
         <TabsContent
-          value="default"
+          value="main"
           className="w-full text-sm md:text-lg text-center hyphens-none py-2 font-['DM Sans'] text-[#d9f8ff] hover:text-white transition-all"
         >
           <DefaultTab />
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,12 +4,10 @@ import { useIsClient, useMediaQuery } from "usehooks-ts";
 import { DashboardHeader, Header } from "./(components)";
 import DashboardProvider from "@/components/providers/dashboard-provider";
 import { Toaster } from "@/components/ui/toaster";
+import DefaultTab from "./(components)/(tabs)/DefaultTab";
 
-type Props = {};
-
-const Dashboard = (props: Props) => {
+const Dashboard = () => {
   const isMobile = useMediaQuery("(max-width: 990px)");
-  const isEXTRASMALL = useMediaQuery("(max-width: 370px)");
   const isClient = useIsClient();
 
   return (
@@ -25,7 +23,18 @@ const Dashboard = (props: Props) => {
                 "radial-gradient(50% 50% at 50% 50%, rgba(119, 186, 234, 0.2), transparent ), radial-gradient( 50% 50% at 50% 50%, rgba(251, 0, 196, 0) 3.49%, rgba(119, 186, 234, 0) 7.6%, rgba(253, 0, 197, 0) 10.46%, rgba(119, 186, 234, 0) 14.46%, rgba(255, 0, 199, 0) 18.56%, rgba(3, 0, 3, 0) 19.53%, transparent 79.82%, rgba(246, 0, 192, 0) 81.08%, rgba(119, 186, 234, 0) 84.04%, rgba(247, 0, 193, 0) 86.61%, rgba(119, 186, 234, 0) 91.01%, rgba(249, 0, 194, 0) 95.16%, rgba(119, 186, 234, 0) 98.6% )",
             }}
           />
-          {isClient && <DashboardHeader isMobile={isEXTRASMALL} />}
+          {isClient && (
+            <div>
+              <DashboardHeader />
+              <div
+                className="w-full
+               text-sm
+               md:text-lg text-center hyphens-none py-2 font-['DM Sans'] text-[#d9f8ff] hover:text-white transition-all"
+              >
+                <DefaultTab />
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </DashboardProvider>
