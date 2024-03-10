09ff5f272ccb93dbd81f40f156a02cea05f5897b
Hamid Reza Lashgari <73655719+HamidrezaLashgari@users.noreply.github.com>
2024-03-10 12:53:59 +0330

    feat: add nft modal

--- app/dashboard/(components)/(tabs)/(blocks)/nft.tsx
+++ app/dashboard/(components)/(tabs)/(blocks)/nft.tsx
@@ -12,8 +12,15 @@ import { useToast } from "@/components/ui/use-toast";
 import formatedNumber from "@/lib/numbers";
 import formatedString, { removeMiddleString } from "@/lib/string";
 import { useWallet } from "@solana/wallet-adapter-react";
-import Image from "next/image";
 import { useEffect, useState } from "react";
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { ChevronFirst, X } from "lucide-react";
+import Image from "next/image";
 
 type Props = {
   isEXTRASMALL: boolean;
@@ -35,6 +42,10 @@ const NFT = ({ isEXTRASMALL }: Props) => {
   const { publicKey } = useWallet();
   const [NFTValue, setNFTValue] = useState(12500);
   const { toast } = useToast();
+
+  const [detailModal, setDetailModal] = useState(false);
+
+  const [burnModal, setBurnModal] = useState(false);
 
   useEffect(() => {
     gdata.length === 0 &&
@@ -157,7 +168,7 @@ const NFT = ({ isEXTRASMALL }: Props) => {
                       <Button
                         size="sm"
                         className="hover:bg-[#7c7c8d80] max-h-8 text-[14px] rounded-full"
-                        onClick={() => console.log("Hey there!")}
+                        onClick={() => setDetailModal(true)}
                         style={{ boxShadow: "0 0 4px 1px #d9f8ff" }}
                       >
                         Details
@@ -170,8 +181,177 @@ const NFT = ({ isEXTRASMALL }: Props) => {
           </TableBody>
         </Table>
       </div>
+      <AlertDialog
+        open={detailModal}
+        onOpenChange={() => setDetailModal(!detailModal)}
+      >
+        <AlertDialogContent
+          className="border-none bg-[#0d111b] rounded-3xl h-max md:px-14"
+          onPointerDown={(event) => setDetailModal(false)}
+          style={{
+            boxShadow: "0 0 20px 1px rgba(217, 248, 255, 0.25)",
+            borderRadius: 24,
+          }}
+        >
+          <AlertDialogHeader>
+            <AlertDialogTitle
+              className="flex justify-between items-center gap-5 p-5 text-[#d9f8ff] bg-[#0d111b55] px-5 rounded-full h-12"
+              style={{
+                boxShadow: "0 0 5px #d9f8ff",
+              }}
+            >
+              <p className="text-sm">Ustur CSS Tier 1 (CSSLU1)</p>
+
+              <Button
+                size="icon"
+                className={"bg-[#0d111b55] rounded-[50%]"}
+                onClick={() => setDetailModal(false)}
+              >
+                <X className="w-6 h-6 aspect-square object-contain" />
+              </Button>
+            </AlertDialogTitle>
+            <div className={"flex-row justify-self-center mx-auto"}>
+              <Image
+                alt="NFT Image"
+                className="w-[340px] h-[190px] relative my-12"
+                src="https://fakeimg.pl/340x340"
+                width={340}
+                height={340}
+              />
+            </div>
+          </AlertDialogHeader>
+
+          <div className="flex flex-row w-full">
+            <div className={"w-full"}>
+              {manageNftItem.map((item, index) => (
+                <p className="text-sm text-sky-100" key={index}>
+                  {item}
+                </p>
+              ))}
+            </div>
+            <div className={"flex flex-col items-end w-full"}>
+              <p className="text-sm text-gray-500">1</p>
+              <p className="text-sm text-gray-500">CSSLU1</p>
+              <p className="text-sm text-gray-500">View Original</p>
+              <p className="text-sm text-gray-500">167</p>
+              <p className="text-sm text-gray-500">Star Atlas</p>
+              <p className="text-sm text-sky-100">BVJE...VQ1V</p>
+              <p className="text-sm text-sky-100">BVJE...VQ1V</p>
+              <p className="text-sm text-gray-500">Primary-MarketMutable</p>
+              <p className="text-sm text-gray-500">0%</p>
+            </div>
+          </div>
+          <div className={"mt-2"}>
+            <p className={"text-gray-500 text-sm"}>Description</p>
+            <p className={"text-gray-500 text-sm"}>
+              Property title for a tier I residential land plot on the surface
+              area of the Ustur Central Space Station. This title was issued
+              under the Council of peace authority during the SAGE Start
+              Sequence Phase II - 2022. Disclaimer: Images are Sogmian District
+              for example only. Final art will vary based on final faction and
+              district selection.
+            </p>
+          </div>
+          <div className={"flex flex-row gap-x-4 mt-4 "}>
+            <Button
+              className={
+                "text-[#d9f8ff] bg-[#0d111b55] px-5 rounded-full w-full"
+              }
+              style={{ boxShadow: "0 0 5px #d9f8ff" }}
+              onClick={() => console.log("Send")}
+            >
+              Send
+            </Button>
+            <Button
+              className={
+                "text-[#d9f8ff] bg-[#0d111b55] px-5 rounded-full w-full"
+              }
+              style={{ boxShadow: "0 0 5px #d9f8ff" }}
+              onClick={() => {
+                setBurnModal(true);
+                setDetailModal(false);
+              }}
+            >
+              Burn
+            </Button>
+          </div>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      {/*  ===================================================================== */}
+
+      <AlertDialog
+        open={burnModal}
+        onOpenChange={() => setBurnModal(!burnModal)}
+      >
+        <AlertDialogContent
+          className="border-none bg-[#0d111b] rounded-3xl h-max md:px-14"
+          onPointerDown={(event) => setBurnModal(false)}
+          style={{
+            boxShadow: "0 0 20px 1px rgba(217, 248, 255, 0.25)",
+            borderRadius: 24,
+          }}
+        >
+          <AlertDialogHeader>
+            <AlertDialogTitle
+              className="flex justify-between items-center gap-5 p-5 text-[#d9f8ff] bg-[#0d111b55] px-5 rounded-full h-12"
+              style={{
+                boxShadow: "0 0 5px #d9f8ff",
+              }}
+            >
+              <p className="text-sm text-red-700">Burn</p>
+              <p className="text-sm">Ustur CSS Tier 1 (CSSLU1)</p>
+              <Button
+                size="icon"
+                className={"bg-[#0d111b55] rounded-[50%]"}
+                onClick={() => setBurnModal(false)}
+              >
+                <X className="w-6 h-6 aspect-square object-contain" />
+              </Button>
+            </AlertDialogTitle>
+            <div className={"flex-row justify-self-center mx-auto"}>
+              <Image
+                alt="NFT Image"
+                className="w-[300px] h-[190px] relative my-12"
+                src="https://fakeimg.pl/340x340"
+                width={340}
+                height={340}
+              />
+            </div>
+          </AlertDialogHeader>
+          <div
+            className="text-gray-500 bg-[#0d111b55] rounded-[25px] p-3"
+            style={{
+              boxShadow: "0 0 5px #d9f8ff",
+            }}
+          >
+            <p>
+              This action will permanently destroy and remove these tokens from
+              your wallet.
+            </p>
+
+            <div>
+              <div>
+                <p>323</p>
+              </div>
+            </div>
+          </div>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 };
 
 export default NFT;
+
+const manageNftItem = [
+  "Balance",
+  "Symbol",
+  "Image",
+  "Current Supply",
+  "Collection",
+  "Mint",
+  "Freeze Authority",
+  "Status",
+  "Royalty",
+];
